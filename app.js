da7d8c1326ddb99cd611d1344a9067e93f90dd64
akif-onder <100137858+akif-onder@users.noreply.github.com>
2022-06-11 10:12:03 +0300

    edited

--- app.js
+++ app.js
@@ -4,3 +4,27 @@ let calculator_input_Number_Previews_V = document.getElementById('calculator_inp
 let Calculator_input_AC_V = document.getElementById('Calculator_input_AC');
 
 let DisplayValue;
+
+function display(DisplayValue){
+    calculator_input_Number_V.value+=DisplayValue;
+
+}
+
+Calculator_input_AC_V.addEventListener('click',()=>{
+    calculator_input_Number_V.value="";
+    calculator_input_Number_Previews_V.value="";
+
+})
+
+function displaySolve(){
+
+    let eq = calculator_input_Number_V.value;
+
+    calculator_input_Number_Previews_V.value=eq;
+
+    let y= eval(eq);
+
+    calculator_input_Number_V.value=y;
+
+
+}
