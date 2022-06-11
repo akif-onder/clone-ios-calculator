4917eb96088182bf5e77bb6bb924f3631413488e
akif-onder <100137858+akif-onder@users.noreply.github.com>
2022-06-11 09:00:01 +0300

    first js commit

---
+++ app.js
@@ -0,0 +1,6 @@
+let calculator_input_Number_V = document.getElementById('calculator_input_Number');
+let calculator_input_Number_Previews_V = document.getElementById('calculator_input_Number_Previews');
+
+let Calculator_input_AC_V = document.getElementById('Calculator_input_AC');
+
+let DisplayValue;
